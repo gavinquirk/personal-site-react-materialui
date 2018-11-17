532a908be4da5d1c0b63747ca61a539338d068c9
Gavin Quirk <gavinquirk@gmail.com>
2018-11-16 20:25:51 -0800

    added packages and basic file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Contact from './pages/Contact'
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <div>
+            <Route path='/' component={Home} exact />
+            <Route path='/contact' component={Contact} />
+            <Route path='/portfolio' component={Portfolio} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
